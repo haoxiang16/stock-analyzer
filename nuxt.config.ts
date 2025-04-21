40b2e1c1d3e219bf954ccf65d509cf9ebc668e8f
haoxiang16 <81564640+haoxiang16@users.noreply.github.com>
2025-04-21 23:33:51 +0800

    add nitro setting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  ssr: true,
+  nitro: {
+    preset: 'netlify'
+  },
   modules: [
     '@nuxt/eslint',
     '@nuxt/icon',
