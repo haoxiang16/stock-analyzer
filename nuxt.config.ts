eadf9b5fac92e775e8318241bfca754efae4b3c1
haoxiang16 <81564640+haoxiang16@users.noreply.github.com>
2025-04-22 11:44:46 +0800

    remove nitro setting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  ssr: true,
-  nitro: {
-    preset: 'netlify'
-  },
   modules: [
     '@nuxt/eslint',
     '@nuxt/icon',
